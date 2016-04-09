3a872c849dcf5fe445f3b3a60c05f2c60c36e014
Adan Silva <adansilvae@gmail.com>
2016-04-10 01:21:20 +0200

    Updating

--- WeCanLeak/Assets/Code/Gameplay/UI/OrganUI.cs
+++ WeCanLeak/Assets/Code/Gameplay/UI/OrganUI.cs
@@ -18,11 +18,44 @@ public class OrganUI : MonoBehaviour {
 
 	private int selectedOrgan;
 
+	private Transform[] liquid1;
+	private Transform[] liquid2;
+	private Transform[] liquid3;
+	private Transform[] liquid4;
+
 	int producerindex;
+
 
 	public void Initialize(int selectedOrgan)
 	{
 		this.selectedOrgan = selectedOrgan;
+
+
+		//Initializing the liquids
+		for (int i = 0; i < 4; i++)
+		{
+
+			for (int j = 0; j < liquid1.Length; j++)
+			{
+
+			}
+		}
+
+		liquid1 = InitializeLiquids(liquid1, juice1);
+		liquid2 = InitializeLiquids(liquid2, juice2);
+		liquid3 = InitializeLiquids(liquid3, juice3);
+		liquid4 = InitializeLiquids(liquid4, juice4);
+
+	}
+
+	private Transform[] InitializeLiquids(Transform[] _liquid, Button _juice)
+	{
+		_liquid = new Transform[3];
+		for (int i = 0; i < _liquid.Length; i++)
+		{
+			_liquid[i] = _liquid[i].gameObject.transform.GetChild(0).GetChild(i).transform;
+		}
+		return _liquid;
 	}
 
 	private void OnEnable()
@@ -44,60 +77,55 @@ public class OrganUI : MonoBehaviour {
 		juice2.onClick.RemoveListener (OnJuice3Clicked);
 		juice2.onClick.RemoveListener (OnJuice4Clicked);
 	}
+
+	//=================================
 	
 	private void OnJuice1Clicked()
 	{
-		if (producerindex == phlegmIndex) //if this organ produces phlegm...
-		{
-			organ.juices[phlegmIndex].amount -= juicePerTap;
-			NetworkComm.ReleaseJuice(JuiceType.Phlegm); //release it
-		}
-		else //if not, request phlegm
-		{
-			if (NetworkComm.RequestJuice(JuiceType.Phlegm)) //might not be available in the pool right now
-				organ.juices[phlegmIndex].amount += juicePerTap;
-		}
+		CalculateJuices(phlegmIndex, liquid1);
 	}
 
 	private void OnJuice2Clicked()
 	{
-		if (producerindex == yellowIndex)
-		{
-			organ.juices[yellowIndex].amount -= juicePerTap;
-			NetworkComm.ReleaseJuice(JuiceType.YellowBile);
-		} 
-		else
-		{
-			if (NetworkComm.RequestJuice(JuiceType.YellowBile))
-				organ.juices[yellowIndex].amount += juicePerTap;
-		}
+		CalculateJuices(yellowIndex, liquid2);
 	}
 
 	private void OnJuice3Clicked()
 	{
-		if (producerindex == blackIndex)
-		{
-			organ.juices[blackIndex].amount -= juicePerTap;
-			NetworkComm.ReleaseJuice(JuiceType.BlackBile);
-		} 
-		else
-		{
-			if (NetworkComm.RequestJuice(JuiceType.BlackBile))
-				organ.juices[blackIndex].amount += juicePerTap;
-		}
+		CalculateJuices(blackIndex, liquid3);
 	}
 
 	private void OnJuice4Clicked()
 	{
-		if (producerindex == bloodIndex)
+		CalculateJuices(bloodIndex, liquid4);
+	}
+
+	//=================================
+
+	private void CalculateJuices(int _thisProducerIndex, Transform[] _liquid)
+	{
+		if (producerindex == _thisProducerIndex) //if this organ produces this Juice/Liquid
 		{
-			organ.juices[bloodIndex].amount -= juicePerTap;
-			NetworkComm.ReleaseJuice(JuiceType.Blood);
-		} 
-		else
+			organ.juices[_thisProducerIndex].amount -= juicePerTap;
+			for(int i = 0; i < _liquid.Length; i++) // Decreases the liquid level in the container
+			{
+				_liquid[i].localScale = new Vector3(0, _liquid[i].localScale.y - 0.1f, 0);
+			}
+			NetworkComm.ReleaseJuice( (JuiceType) phlegmIndex ); //release it
+		}
+		else //if not, request phlegm
 		{
-			if (NetworkComm.RequestJuice(JuiceType.Blood))
-				organ.juices[bloodIndex].amount += juicePerTap;
+			if (NetworkComm.RequestJuice( (JuiceType) phlegmIndex )) //might not be available in the pool right now
+			{
+				organ.juices[_thisProducerIndex].amount += juicePerTap;
+				for(int i = 0; i < _liquid.Length; i++) // Increases the liquid level in the container
+				{
+					_liquid[i].localScale = new Vector3(0, _liquid[i].localScale.y + 0.1f, 0);
+				}
+			}
 		}
 	}
+
+
+
 }
