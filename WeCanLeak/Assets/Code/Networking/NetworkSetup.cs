adec6e9fcd9d6c6911af10a304d65a9c1826c714
Andrea Catalini <andrea.catalini89@gmail.com>
2016-04-09 13:30:04 +0200

    Initial folder structure, FOR NOW.

---
+++ WeCanLeak/Assets/Code/Networking/NetworkSetup.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkSetup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings("v4.2");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
