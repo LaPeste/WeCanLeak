7c0f94508514a379f94bf9a16cced79c04621f40
Lukas Kern <lukas.kern2@gmail.com>
2016-04-09 18:31:26 +0200

    progress

--- WeCanLeak/Assets/Code/Gameplay/UI/OrganUI.cs
+++ WeCanLeak/Assets/Code/Gameplay/UI/OrganUI.cs
@@ -1,49 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class OrganUI : MonoBehaviour {
 
-	public Button juice1;
-	public Button juice2;
-	public Button juice3;
-	public Button juice4;
 	private Organ organ;
+	public List<Button> buttons;
 
 	private void OnEnable()
 	{
 		organ = Resources.Load<GameObject> ("OrganSettings").GetComponent<OrganSettings>().organs[0];
-
-		juice1.onClick.AddListener (OnJuice1Clicked);
-		juice2.onClick.AddListener (OnJuice2Clicked);
-		juice3.onClick.AddListener (OnJuice3Clicked);
-		juice4.onClick.AddListener (OnJuice4Clicked);
+		foreach (Button b in buttons)
+		{
+			if (buttons.FindIndex 
+			b.onClick.AddListener (OnProducerClicked)
+			b.onClick.AddListener (OnConsumerClicked)
+		}
 	}
 	
 	private void OnDisable()
 	{
-		juice1.onClick.RemoveListener (OnJuice1Clicked);
-		juice2.onClick.RemoveListener (OnJuice2Clicked);
-		juice2.onClick.RemoveListener (OnJuice3Clicked);
-		juice2.onClick.RemoveListener (OnJuice4Clicked);
+		
 	}
 	
-	private void OnJuice1Clicked()
-	{
-
-	}
-
-	private void OnJuice2Clicked()
+	private void OnConsumerClicked()
 	{
 		
 	}
 
-	private void OnJuice3Clicked()
-	{
-		
-	}
-
-	private void OnJuice4Clicked()
+	private void OnProducerClicked()
 	{
 		
 	}
