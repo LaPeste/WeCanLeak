f8e119e496f6eecfe599070c8b41845ffc167e8d
Till Riemer <till.riemer@gmail.com>
2016-04-09 15:59:15 +0200

    Start Screen UI

---
+++ WeCanLeak/Assets/Code/Gameplay/UI/StartScreenUI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StartScreenUI : MonoBehaviour {
+
+	public Button fullbodyButton;
+	public Button bodyPartButton;
+
+	public GameObject bodyUIPrefab;
+	public GameObject bodyPartUIPrefab;
+
+	private void OnEnable()
+	{
+		fullbodyButton.onClick.AddListener (OnFullBodyClicked);
+		bodyPartButton.onClick.AddListener (OnBodyPartClicked);
+	}
+
+	private void OnDisable()
+	{
+		fullbodyButton.onClick.RemoveListener (OnFullBodyClicked);
+		bodyPartButton.onClick.RemoveListener (OnBodyPartClicked);
+	}
+
+	private void OnFullBodyClicked()
+	{
+		Instantiate (bodyUIPrefab);
+		Destroy (gameObject);
+	}
+
+	private void OnBodyPartClicked()
+	{
+		Instantiate (bodyUIPrefab);
+		Destroy (bodyPartUIPrefab);
+	}
+}
